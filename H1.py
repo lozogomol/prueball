7424aa96029773a47398a216ab9f0ad6a6fafd4e
llozogomoll <llozogomoll@gmail.com>
2026-02-24 15:04:38 -0400

    v1

---
+++ H1.py
@@ -0,0 +1,16 @@
+K = int(input())
+
+
+plata_total = 0
+for dia in range(1, K + 1):
+    plata_total += dia
+
+
+
+oro_total = 0
+for dia in range(2, K + 1, 2):
+    oro_total += dia
+
+
+desbalance = abs(oro_total - plata_total)
+{oro_total} - {plata_total}| = {desbalance}")
